41baed7dd0e53b7d4fdc2ef3984406e08e7e42d2
framadhan99 <fajarramadhan99704@gmail.com>
2023-03-12 21:24:18 +0700

    finally

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -28,12 +28,90 @@ class _HomePageState extends State<HomePage> {
     snakePosition = [45, 65, 85, 105, 125];
     const duration = Duration(milliseconds: 300);
     Timer.periodic(duration, (timer) {
-      // updateSnake();
+      updateSnake();
+      if (gameOver()) {
+        timer.cancel();
+        _showGameOverScreen();
+      }
     });
   }
 
   var direction = 'down';
-  // void updateSnake
+  void updateSnake() {
+    setState(() {
+      switch (direction) {
+        case 'down':
+          if (snakePosition.last > 740) {
+            snakePosition.add(snakePosition.last + 20 + 760);
+          } else {
+            snakePosition.add(snakePosition.last + 20);
+          }
+          break;
+        case 'up':
+          if (snakePosition.last < 20) {
+            snakePosition.add(snakePosition.last - 20 + 760);
+          } else {
+            snakePosition.add(snakePosition.last - 20);
+          }
+          break;
+        case 'left':
+          if (snakePosition.last % 20 == 0) {
+            snakePosition.add(snakePosition.last - 1 + 20);
+          } else {
+            snakePosition.add(snakePosition.last - 1);
+          }
+          break;
+        case 'right':
+          if ((snakePosition.last + 1) % 20 == 0) {
+            snakePosition.add(snakePosition.last + 1 + 20);
+          } else {
+            snakePosition.add(snakePosition.last + 1);
+          }
+          break;
+        default:
+      }
+      if (snakePosition.last == food) {
+        generatedNewFood();
+      } else {
+        snakePosition.removeAt(0);
+      }
+    });
+  }
+
+  bool gameOver() {
+    for (int i = 0; i < snakePosition.length; i++) {
+      int count = 0;
+      for (int j = 0; j < snakePosition.length; j++) {
+        if (snakePosition[i] == snakePosition[j]) {
+          count += 1;
+        }
+        if (count == 2) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  void _showGameOverScreen() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Game over'),
+            content: Text("You'r Score:" + snakePosition.length.toString()),
+            actions: [
+              MaterialButton(
+                onPressed: () {
+                  startGame();
+                  Navigator.pop(context);
+                },
+                child: Text('Play Again'),
+              )
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,26 +120,78 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
           Expanded(
-              child: GestureDetector(
-            child: Container(
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 20),
-                itemBuilder: (context, index) {
-                  return Center(
-                    child: Container(
-                      // color: Colors.green,
-                      padding: EdgeInsets.all(2),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(5),
-                        child: Container(color: Colors.white),
-                      ),
-                    ),
-                  );
-                },
+            child: GestureDetector(
+              onVerticalDragUpdate: (details) {
+                if (direction != 'up' && details.delta.dy > 0) {
+                  direction = 'down';
+                } else if (direction != 'down' && details.delta.dy < 0) {
+                  direction = 'up';
+                }
+              },
+              onHorizontalDragUpdate: (details) {
+                if (direction != 'left' && details.delta.dx > 0) {
+                  direction = 'right';
+                } else if (direction != 'right' && details.delta.dx < 0) {
+                  direction = 'left';
+                }
+              },
+              child: Container(
+                child: GridView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: numberOfSquares,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 20),
+                  itemBuilder: (context, index) {
+                    if (snakePosition.contains(index)) {
+                      return Center(
+                        child: Container(
+                          // color: Colors.green,
+                          padding: EdgeInsets.all(2),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(5),
+                            child: Container(color: Colors.red[400]),
+                          ),
+                        ),
+                      );
+                    }
+                    if (index == food) {
+                      return Container(
+                        padding: const EdgeInsets.all(2),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(5),
+                          child: Container(color: Colors.white),
+                        ),
+                      );
+                    } else {
+                      return Container(
+                        padding: const EdgeInsets.all(2),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(5),
+                          child: Container(color: Colors.grey[900]),
+                        ),
+                      );
+                    }
+                  },
+                ),
               ),
             ),
-          ))
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 20, left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: startGame,
+                  child: Text(
+                    's t a r t',
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
